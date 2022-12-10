87abe67379a1abb2a7e613ff6a9f14c9bc01d218
Proka01 <aleksa.prokic888@gmail.com>
2022-12-10 12:07:05 +0100

    dijkstra algorithm gameGenerator modification

---
+++ dijkstra.py
@@ -0,0 +1,70 @@
+import heapq
+import math
+
+
+def dijskstra(start_q, start_r, game_hex_map, players_map, my_id):
+    move_q = [0, 1, 1, 0, -1, -1]
+    move_r = [-1, -1, 0, 1, 1, 0]
+
+    min_heap = []
+    dist_map = {}
+    heapq.heappush(min_heap, (0, start_q, start_r))
+
+    for tile in game_hex_map.keys():
+        dist_map[tile] = 10 ** 9
+
+    dist_map[f'{start_q}:{start_r}'] = 0
+
+    while min_heap:
+        d, q, r = heapq.heappop(min_heap)
+
+        key = f'{q}:{r}'
+
+        if key not in game_hex_map.keys():
+            continue
+
+        if d > dist_map[key]:
+            continue
+
+        for i in range(0, 6):
+            qq = q + move_q[i]
+            rr = r + move_r[i]
+
+            next_key = f'{qq}:{rr}'
+
+            if next_key not in game_hex_map.keys():
+                continue
+
+            if game_hex_map[next_key]['type'] == 'ASTEROID':
+                w = math.ceil(350.0 / players_map[my_id]['power']) + 1
+            elif game_hex_map[next_key]['type'] == 'BLACKHOLE':
+                w = 3
+            else:
+                w = 1
+
+            if game_hex_map[next_key]['type'] != 'WORMHOLE':
+                if dist_map[key] + w < dist_map[next_key]:
+                    dist_map[next_key] = dist_map[key] + w
+                    heapq.heappush(min_heap, (dist_map[next_key], qq, rr))
+            else:
+                qq, rr = game_hex_map[next_key]['teleportsTo'].split(':')
+                qq = int(qq) + move_q[i]
+                rr = int(rr) + move_r[i]
+                if dist_map[key] + w < dist_map[f'{qq}:{rr}']:
+                    dist_map[f'{qq}:{rr}'] = dist_map[key] + w
+                    heapq.heappush(min_heap, (dist_map[f'{qq}:{rr}'], qq, rr))
+
+
+            # if dist_map[key] + w < dist_map[next_key]:
+            #     dist_map[next_key] = dist_map[key] + w
+            #     if game_hex_map[next_key]['type'] != 'WORMHOLE':
+            #         heapq.heappush(min_heap, (dist_map[next_key], qq, rr))
+            #     else:
+            #         qq, rr = game_hex_map[next_key]['teleportsTo'].split(':')
+            #         qq = int(qq) + move_q[i]
+            #         rr = int(rr) + move_r[i]
+            #         dist_map[f'{qq}:{rr}'] = dist_map[key] + w
+            #         heapq.heappush(min_heap, (dist_map[f'{qq}:{rr}'], qq, rr))
+
+    print(dist_map)
+    return dist_map
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import serverCommunication
 import gameGenerator
+import dijkstra
 import json
 
 if __name__ == '__main__':
-    token = serverCommunication.login()
+    # token = serverCommunication.login()
     # serverCommunication.game_train(token,"test1.txt",1,8)
 
-    game_state_json2 = open('game_state.json')
+    game_state_json2 = open('game_state_json2.json')
+    content = game_state_json2.read()
+    players_map = gameGenerator.create_player_info(content)
+    game_hex_map = gameGenerator.create_hexagon_game_map(content)
+    dijkstra.dijskstra(-7, -7, game_hex_map, players_map, 1)
 
-    gameGenerator.create_hexagon_game_map(game_state_json2.read())
     #serverCommunication.join_game(token)
     #serverCommunication.game_do_action(token,"move","-6","-7")
     # serverCommunication.game_action_train(token, "move", "-7", "-7")
